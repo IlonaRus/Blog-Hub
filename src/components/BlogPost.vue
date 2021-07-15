6a2fef0bba829a3e7fc4ede6f012576d2a85ac8f
Ilona <ilonarus@hotmail.com>
2021-07-15 19:33:56 +0200

    Added blogpost component

---
+++ src/components/BlogPost.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="blog-wrapper">
+    <div class="blog-content">
+      <div>
+        <h2 v-if="post.welcomeScreen">{{ post.title }}</h2>
+        <h2 v-else>{{ post.title }}</h2>
+        <p v-if="post.welcomeScreen">{{ post.blogPost }}</p>
+        <p class="content-preview" v-else>{{ post.blogHTML }}</p>
+        <router-link class="link link-light" v-if="post.welcomeScreen" to="#">
+          Login/Register
+          <Arrow class="arrow arrow-light" />
+        </router-link>
+        <router-link v-else></router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Arrow from "../assets/Icons/arrow-right-light.svg"
+
+export default {
+  name: "blogPost",
+  props: ["post"],
+  components: {
+    Arrow,
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
