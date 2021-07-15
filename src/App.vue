9bd6a24d34071a07fd94dc29ce74c2b279fac1eb
Ilona <ilonarus@hotmail.com>
2021-07-15 16:31:07 +0200

    Added footer

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,21 @@
     <div class="app">
       <Navigation />
       <router-view />
+      <Footer />
     </div>
   </div>
 </template>
 
 <script>
-import Navigation from './components/Navigation'
+import Navigation from './components/Navigation';
+import Footer from './components/Footer';
+
 export default {
   name: "app",
-  components: { Navigation },
+  components: { 
+    Navigation,
+    Footer 
+  },
   data() {
     return {};
   },
