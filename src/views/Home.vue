e3f83fd778a3f5612b83df4d0ef3f59745b6e5a8
Ilona <ilonarus@hotmail.com>
2021-07-15 19:34:07 +0200

    Added homepage content

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,37 @@
 <template>
   <div class="home">
+    <BlogPost :post="welcomeScreen" />
+    <BlogPost :post="blogpost" v-for="(blogpost, index) in sampleBlogPost" :key="index"/>
   </div>
 </template>
 
 <script>
+import BlogPost from '../components/BlogPost';
 export default {
   name: "Home",
-  components: {},
+  components: { BlogPost },
+  data() {
+    return {
+      welcomeScreen: {
+        title: "Welcome!",
+        blogPost:
+          "Weekly blog articles with all things programming including HTML, CSS, JavaScript and more. Register today to never miss a post!",
+        welcomeScreen: true,
+        photo: "coding",
+      },
+      sampleBlogPost: [
+        {
+          title: "This is a filler title!",
+          blogHTML: "This is a filler blog post title",
+          blogCoverPhoto: "beautiful-stories",
+        },
+        {
+          title: "This is a filler title!",
+          blogHTML: "This is a filler blog post title",
+          blogCoverPhoto: "designed-for-everyone",
+        },
+      ],
+    };
+  },
 };
 </script>
