11e3210afd96c5cad3fa9679b4ae1aeaba45031e
Ilona <ilonarus@hotmail.com>
2021-08-06 16:19:34 +0200

    Created forgot password page

---
+++ src/views/ForgotPassword.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="reset-password">
+    <Modal v-if="modalActive" v-on:close-modal="closeModal" />
+    <Loading v-if="loading" />
+    <div class="form-wrap">
+      <form class="reset">
+        <h2>Reset password</h2>
+        <p>Forgot your password? Enter your e-mail to reset it</p>
+        <div class="inputs">
+          <div class="input">
+            <input type="text" placeholder="E-mail" v-model="email">
+            <email class="icon" />
+          </div>
+        </div>
+        <button>Reset</button>
+      </form>
+      <div class="background"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import email from "../assets/Icons/envelope-regular.svg";
+import Modal from "../components/Modal.vue";
+import Loading from '../components/Loading.vue';
+
+export default {
+  name: "ForgotPassword",
+  data() {
+    return {
+      email: null,
+      modalActive: false,
+      modalMessage: "",
+      loading: null,
+    }
+  },
+  components: {
+    email,
+    Modal,
+    Loading,
+  },
+  methods: {
+    closeModal() {
+      this.modalActive = !this.modalActive;
+      this.email = "";
+    }
+  }
+
+};
+</script>
+
+<style lang="scss" scoped>
+.reset-password {
+  position: relative;
+
+  .form-wrap {
+    .reset {
+      h2 {
+        margin-bottom: 8px;
+      }
+
+      p {
+        text-align: center;
+        margin-bottom: 32px;
+      }
+    }
+  }
+}
+
+</style>
